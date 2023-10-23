2a7dd82182b1fe7ed468ff5ef8bcf93ec1fa3b79
Jeremias Erba <jeremiaserba7894@gmail.com>
2023-10-23 15:55:02 -0300

    Task 3 and 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,42 @@
 #!/usr/bin/python3
 """ Imports """
+import uuid
+from datetime import datetime
+import models
 
 
 class BaseModel:
     """
         BaseModel Module
     """
-    pass
+    def __init__(self, *args, **kwargs):
+        """Init Value"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+
+    def __str__(self):
+        """Str return"""
+        datad = self.__class__.__name__
+        return "[{}] ({}) {}".format(datad, self.id, self.__dict__)
+
+    def save(self):
+        """Save"""
+        models.storage.save()
+        self.update_at = datetime.now()
+
+    def to_dict(self):
+        """Dict values"""
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
